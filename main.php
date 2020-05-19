55136b7b995c8d8530eea46ec8bc34c18448509e
Tubasa Matsuoka <workwing0283@gamil.com>
2020-05-19 17:19:24 +0900

    マルチバイト文字列

--- main.php
+++ main.php
@@ -1,11 +1,11 @@
 <?php
 
-$input = ' dot_taguchi  ';
+$input = ' こんにちは  ';
 $input = trim($input);
 
-// echo strlen($input) . PHP_EOL;
-// echo strpos($input, '_') . PHP_EOL;
+echo mb_strlen($input) . PHP_EOL; // 5
+echo mb_strpos($input, 'に') . PHP_EOL; // 2
 
-$input = str_replace('_', '-', $input);
+$input = str_replace('にち', 'ばん', $input); // こんばんは
 echo $input . PHP_EOL;
 ?>
