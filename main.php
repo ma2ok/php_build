356ae27e794b12f224b828d36586d7bcdd5423b3
Tubasa Matsuoka <workwing0283@gamil.com>
2020-06-04 14:06:58 +0900

    ファイル読み込み

--- main.php
+++ main.php
@@ -1,9 +1,12 @@
 <?php
+// $fp = fopen('names.txt', 'r');
+// $contents = fread($fp, filesize('names.txt'));
+// fclose($fp);
+// echo $contents;
 
-$fp = fopen('names.txt', 'a');
-
-fwrite($fp, "jiro\n");
-fwrite($fp, "saburo\n");
-
+$fp = fopen('names.txt', 'r');
+while (($line = fgets($fp)) !== false) {
+  echo $line;
+}
 fclose($fp);
 ?>
